a355538148df548298e611a6b3b32ead200939ef
humberto <humberto@pactosolucoes.com.br>
2019-10-02 22:22:49 -0300

    Commit inicial alteracoes feitas em sala de aula

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Refatoração e Evolução de Software
 Disciplina de Refatoração e Evolução de Software da pós graduação da UNIALFA
+
+
+
+Fazer um teste no fork
--- catalogo-fowler/extractMethod/Divida.java
+++ catalogo-fowler/extractMethod/Divida.java
@@ -7,11 +7,17 @@ public class Divida {
 	private String nome;
 
 	void imprimeDivida() {
-		// imprime cabeçalho
-		System.out.println("***************************");
-		System.out.println("*** Dívidas do Cliente ****");
-		System.out.println("***************************");
-		// calcula dívidas
+		imprimeCabecalho();
+		double divida = getDivida();
+		imprimeDetalhes(divida);
+	}
+
+	private void imprimeDetalhes(double divida) {
+		System.out.println("nome: " + nome);
+		System.out.println("divida total: " + divida);
+	}
+
+	private double getDivida() {
 		double divida = 0.0;
 		int i = 0;
 		ArrayList<Itens> e = pedidos.getPedidos();
@@ -19,8 +25,12 @@ public class Divida {
 			Itens cada = (Itens) e.get(i);
 			divida += cada.getValor();
 		}
-		// imprime detalhes
-		System.out.println("nome: " + nome);
-		System.out.println("divida total: " + divida);
+		return divida;
+	}
+
+	private void imprimeCabecalho() {
+		System.out.println("***************************");
+		System.out.println("*** Dívidas do Cliente ****");
+		System.out.println("***************************");
 	}
 }
